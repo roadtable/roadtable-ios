f01446c5c8604e94e27cc3aa0683ccb1defddcb4
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-03 12:50:24 -0400

    Get and post requests working. Added TableViewController and RestaurantTableViewCell. Need to pass get request to table view.

---
+++ RoadTableApp/Restaurant.swift
@@ -0,0 +1,22 @@
+//
+//  Restaurant.swift
+//  RoadTableApp
+//
+//  Created by Daniel Nathan Beyrer on 9/3/15.
+//  Copyright (c) 2015 theironyard. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+//class Restaurant {
+//    
+//    var name : String
+//    var rating_img_url : String
+//    var categories : [String]
+//    
+//    
+//    
+//    
+//}
---
+++ RoadTableApp/RestaurantTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  RestaurantTableViewController.swift
+//  RoadTableApp
+//
+//  Created by Daniel Nathan Beyrer on 9/3/15.
+//  Copyright (c) 2015 theironyard. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class RestaurantTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request(.GET, "http://roadtable.herokuapp.com")
+            .responseJSON { (request, response, data, error) in
+                if let anError = error {
+                    // got an error in getting the data, need to handle it
+                    println("error calling POST on /posts")
+                    println(error)
+                } else if let data: AnyObject = data {
+                    let session = JSON(data)
+                    self.loadRestaurants( session )
+                }
+        }
+    }
+    
+    func loadRestaurants( session : JSON ) {
+        for (session, restaurant) in session["restaurants"] {
+            println(restaurant["name"])
+        }
+        println(session["restaurants"].count)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

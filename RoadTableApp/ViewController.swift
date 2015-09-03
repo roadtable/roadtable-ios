49308e9849276f2f3d4a23671ae95b60639259ff
Caleb Francis <calebonesimus@gmail.com>
2015-09-02 22:07:51 -0400

    Initial commit. Has working post request. Can obtain JSON

--- RoadTableApp/ViewController.swift
+++ RoadTableApp/ViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createSession("indianapolis", destination: "bloomington")
+        
+        
     }
+    
+    
+    
+    
+    func createSession(origin: String, destination: String) {
+        Alamofire.request(.POST, "http://roadtable.herokuapp.com/sessions", parameters: ["origin":"\(origin)",                                       "destination":"\(destination)"], encoding: .JSON)
+            .responseJSON { (request, response, data, error) in
+                if let anError = error {
+                    // got an error in getting the data, need to handle it
+                    println("error calling POST on /posts")
+                    println(error)
+                } else if let data: AnyObject = data {
+                    let session = JSON(data)
+                    for (name, info) in session["restaurants"] {
+                        println(name)
+                        println(info["rating"])
+                    }
+                    println("done!")
+                }
+        }
+        
+        
+    } // end createSession()
+ 
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
